6192ae8ae65c8e588179d8b41871fdeeb6a8dba6
Aditya Goswami <aditya.goswami2025@gmail.com>
2025-02-09 18:56:44 +0530

    first commit

---
+++ src/Config/gemini.js
@@ -0,0 +1,36 @@
+
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyCf6fBcsKPYC5pBBDCMb9 -06EJVsY4isQ0";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function runChat(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text();
+  }
+  
+export default runChat;
